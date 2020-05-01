d221e8a6148c5acd777b8b6d956bc6c108db0ba4
bporter11 <bporter11@student.cccd.edu>
2020-05-01 02:13:38 -0700

    First commit

--- Project1/CharacterType.h
+++ Project1/CharacterType.h
@@ -12,22 +12,34 @@ class CharacterType
 {
 public:
 	// Default constructor
-  		
-	// Overloaded constructor
-		
-	// setCharacterInfo
-		
-	// getFirstName
-	// getLastName
-	// getID
+	CharacterType() { id = 0; }
 
+	// Overloaded constructor
+	CharacterType(std::string& newFirstName, std::string& newLastName, int newId);
+
+	// setCharacterInfo
+	void setCharacterInfo(std::string& newFirstName, std::string& newLastName, int newId);
+
+	// getFirstName
+	std::string getFirstName() const;
+
+	// getLastName
+	std::string getLastName() const;
+
+	// getID
+	int getID() const;
 
 	// printName
+	void printName() const;
+
 	// printCharacterInfo
+	void printCharacterInfo() const;
+
 	// printID
+	void printID() const;
 
 	// Destructor
-
+	~CharacterType();
 private:
 	std::string firstName;	
 	std::string lastName;	
